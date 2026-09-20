5a0809109f08447260cb6b183af7ce1fe5a7bb15
arantes225 <luizarantes2255@gmail.com>
2026-09-20 19:51:26 -0300

    pwa: invalida cache apos corrigir sidebar

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "luria-pwa-v14";
+const CACHE_VERSION = "luria-pwa-v15";
 const STATIC_CACHE = CACHE_VERSION + "-static";
 const RUNTIME_CACHE = CACHE_VERSION + "-runtime";
 
@@ -12,7 +12,7 @@ const APP_SHELL = [
   "/assets/css/landing.css",
   "/assets/css/pwa-mobile.css?v=9",
   "/assets/css/luria-brand-v5.css",
-  "/assets/js/app.js?v=14.2",
+  "/assets/js/app.js?v=14.3",
   "/assets/js/pwa.js",
   "/assets/img/favicon.png",
   "/assets/img/logo-icone-original.png",
