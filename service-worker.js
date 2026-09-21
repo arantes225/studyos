1f8a579386d5ee843a56dc909cdee9478b0263a9
arantes225 <luizarantes2255@gmail.com>
2026-09-21 18:40:09 -0300

    Atualiza cache do foguinho Studyrats

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "luria-pwa-v83";
+const CACHE_VERSION = "luria-pwa-v84";
 const STATIC_CACHE = CACHE_VERSION + "-static";
 const RUNTIME_CACHE = CACHE_VERSION + "-runtime";
 
@@ -20,7 +20,7 @@ const APP_SHELL = [
   "/assets/css/onboarding.css?v=1.9",
   "/assets/js/storage-router.js?v=1.0.0",
   "/assets/js/pwa.js",
-  "/assets/css/amigos.css?v=24",
+  "/assets/css/amigos.css?v=25",
   "/assets/js/studyrats.js?v=22",
   "/assets/img/studyrats/ratinhos/ratinho_verde_radioativo.webp?v=16",
   "/assets/img/studyrats/ratinhos/ratinho_laranja.webp?v=16",
