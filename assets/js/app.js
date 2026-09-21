625cc62da268a655c0e1db02170c3bd2a3041ac8
arantes225 <luizarantes2255@gmail.com>
2026-09-21 00:55:53 -0300

    Load fixed onboarding restart flow

--- assets/js/app.js
+++ assets/js/app.js
@@ -3231,7 +3231,7 @@ function carregarOnboardingGlobal() {
       "luria-onboarding-script";
 
     script.src =
-      "/assets/js/onboarding.js?v=1.9";
+      "/assets/js/onboarding.js?v=2.0";
 
     script.onload =
       () => resolve();
