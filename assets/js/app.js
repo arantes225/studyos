5d30206a2d5384aa0bab3a623133daf08b847385
arantes225 <luizarantes2255@gmail.com>
2026-09-21 00:49:43 -0300

    Load onboarding v1.9

--- assets/js/app.js
+++ assets/js/app.js
@@ -3231,7 +3231,7 @@ function carregarOnboardingGlobal() {
       "luria-onboarding-script";
 
     script.src =
-      "/assets/js/onboarding.js?v=1.8";
+      "/assets/js/onboarding.js?v=1.9";
 
     script.onload =
       () => resolve();
