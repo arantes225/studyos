109e39247adfa6575f46ee0c226507ace4d627ad
arantes225 <luizarantes2255@gmail.com>
2026-09-21 13:34:16 -0300

    Cache all six Studyrats PNG assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "luria-pwa-v66";
+const CACHE_VERSION = "luria-pwa-v67";
 const STATIC_CACHE = CACHE_VERSION + "-static";
 const RUNTIME_CACHE = CACHE_VERSION + "-runtime";
 
@@ -20,12 +20,14 @@ const APP_SHELL = [
   "/assets/css/onboarding.css?v=1.9",
   "/assets/js/storage-router.js?v=1.0.0",
   "/assets/js/pwa.js",
-  "/assets/css/amigos.css?v=14",
-  "/assets/js/studyrats.js?v=7",
-  "/assets/img/studyrats/rat-brown.png?v=7",
-  "/assets/img/studyrats/rat-gray.png?v=7",
-  "/assets/img/studyrats/rat-charcoal.png?v=7",
-  "/assets/img/studyrats/rat-white.png?v=7",
+  "/assets/css/amigos.css?v=15",
+  "/assets/js/studyrats.js?v=8",
+  "/assets/img/studyrats/rat-brown.png?v=8",
+  "/assets/img/studyrats/rat-gray.png?v=8",
+  "/assets/img/studyrats/rat-white.png?v=8",
+  "/assets/img/studyrats/rat-black.png?v=8",
+  "/assets/img/studyrats/rat-blue.png?v=8",
+  "/assets/img/studyrats/rat-manchado.png?v=8",
     "/assets/img/favicon.png",
   "/assets/img/logo-icone-original.png",
   "/assets/img/logo-principal.png",
