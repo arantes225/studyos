6d41c635db5b777d3abb7c37c0e69f56d1f38fca
arantes225 <luizarantes2255@gmail.com>
2026-09-22 20:14:51 -0300

    Atualiza cache do onboarding

--- assets/js/app.js
+++ assets/js/app.js
@@ -3575,7 +3575,7 @@ function carregarOnboardingGlobal() {
       "luria-onboarding-script";
 
     script.src =
-      "/assets/js/onboarding.js?v=2.5";
+      "/assets/js/onboarding.js?v=2.6";
 
     script.onload =
       () => resolve();
