3430a65e9ce3fc65121b1a23bf45063d311699d8
arantes225 <luizarantes2255@gmail.com>
2026-09-21 23:51:21 -0300

    Atualiza cache do novo layout Studyrats

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "luria-pwa-v89";
+const CACHE_VERSION = "luria-pwa-v90";
 const STATIC_CACHE = CACHE_VERSION + "-static";
 const RUNTIME_CACHE = CACHE_VERSION + "-runtime";
 
@@ -20,8 +20,8 @@ const APP_SHELL = [
   "/assets/css/onboarding.css?v=1.9",
   "/assets/js/storage-router.js?v=1.0.0",
   "/assets/js/pwa.js",
-  "/assets/css/amigos.css?v=29",
-  "/assets/js/studyrats.js?v=23",
+  "/assets/css/amigos.css?v=34",
+  "/assets/js/studyrats.js?v=28",
   "/assets/img/studyrats/ratinhos/ratinho_verde_radioativo.webp?v=16",
   "/assets/img/studyrats/ratinhos/ratinho_laranja.webp?v=16",
   "/assets/img/studyrats/ratinhos/ratinho_tigrado.webp?v=16",
