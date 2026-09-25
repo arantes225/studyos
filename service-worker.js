04826b572e5dbb5c94c005448b8f1784f4298a04
arantes225 <luizarantes2255@gmail.com>
2026-09-25 02:03:35 -0300

    PWA: publicar correção da anamnese do Plantão

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "luria-pwa-v107-exam-priority";
+const CACHE_VERSION = "luria-pwa-v107-plantao-anamnese-v7";
 const PLANTAO_IMAGE_CACHE = "luria-plantao-images-v1";
 const PLANTAO_IMAGE_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000;
 const STATIC_CACHE = CACHE_VERSION + "-static";
