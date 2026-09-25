2b3fa021e8992770f9b0d852f732d8b072590626
arantes225 <luizarantes2255@gmail.com>
2026-09-25 02:14:08 -0300

    PWA: renovar cache Plantão RPC

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "luria-pwa-v110-plantao-cases";
+const CACHE_VERSION = "luria-pwa-v111-plantao-rpc";
 const PLANTAO_IMAGE_CACHE = "luria-plantao-images-v1";
 const PLANTAO_IMAGE_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000;
 const STATIC_CACHE = CACHE_VERSION + "-static";
