339e87f4b15a04238bd6159393369e8dbe379943
arantes225 <luizarantes2255@gmail.com>
2026-09-25 02:21:28 -0300

    Refresh PWA cache for Plantao render fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "luria-pwa-v111-plantao-rpc";
+const CACHE_VERSION = "luria-pwa-v112-plantao-render";
 const PLANTAO_IMAGE_CACHE = "luria-plantao-images-v1";
 const PLANTAO_IMAGE_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000;
 const STATIC_CACHE = CACHE_VERSION + "-static";
