f94ff0d6f800351c1c373467feca25f9b79d89e0
arantes225 <luizarantes2255@gmail.com>
2026-09-26 17:15:32 -0300

    Refresh PWA rules cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "luria-pwa-v132-plantao-audit";
+const CACHE_VERSION = "luria-pwa-v133-completion-rules";
 const PLANTAO_IMAGE_CACHE = "luria-plantao-images-v1";
 const PLANTAO_IMAGE_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000;
 const STATIC_CACHE = CACHE_VERSION + "-static";
